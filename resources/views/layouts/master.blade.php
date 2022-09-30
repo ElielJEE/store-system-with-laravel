318f3508ceae69a5f85b85d9f87c79b7e9dca805
eliel <eliel123escobar@gmail.com>
2022-09-29 19:46:52 -0600

    ADD: style in layout

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Document</title>
+  @vite(['resources/css/app.css', 'resources/scss/app.scss', 'resources/js/app.js'])
 </head>
 <body>
   <div class="main-container">
@@ -17,41 +18,43 @@
         </div>
         <div class="header-content__username-container">
           <span class="username">
-            juan
+            @juan
           </span>
         </div>
       </div>
     </header>
-    <aside class="sidebar-container">
-      <div class="sidebar-content">
-        <div class="sidebar-content__list">
-          <ul class="sidebar-list">
-            <li class="sidebar-list__item">
-              Inicio
-            </li>
-            <li class="sidebar-list__item">
-              Facturacion
-            </li>
-            <li class="sidebar-list__item">
-              Productos
-            </li>
-            <li class="sidebar-list__item">
-              Categorias
-            </li>
-            <li class="sidebar-list__item">
-              Clientes
-            </li>
-          </ul>
-        </div>
-        <div class="sidebar-content__btn">
-          <button class="btn btn-logout">
-            <span class="btn-text">Cerrar sesion</span>
-          </button>
-        </div>
-      </div>
-    </aside>
     <div class="container">
-      @yield('content')
+      <aside class="sidebar-container">
+        <div class="sidebar-content">
+          <div class="sidebar-content__list">
+            <ul class="sidebar-list">
+              <li class="sidebar-list__item">
+                Inicio
+              </li>
+              <li class="sidebar-list__item">
+                Facturacion
+              </li>
+              <li class="sidebar-list__item">
+                Productos
+              </li>
+              <li class="sidebar-list__item">
+                Categorias
+              </li>
+              <li class="sidebar-list__item">
+                Clientes
+              </li>
+            </ul>
+          </div>
+          <div class="sidebar-content__btn">
+            <button class="btn btn-logout">
+              <span class="btn-text">Cerrar sesion</span>
+            </button>
+          </div>
+        </div>
+      </aside>
+      <div class="center-container">
+        @yield('content')
+      </div>
     </div>
   </div>
 </body>
